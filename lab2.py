f2bbe26f73f23572915a2020c2c277e880d40dee
Dinh Quy Pham <dqpham2@myseneca.ca>
2023-02-15 15:53:32 -0500

    Add python file

---
+++ lab2.py
@@ -0,0 +1,11 @@
+import sys
+
+def print_args():
+    script_name = sys.argv[0]
+    print("Script name:", script_name)
+
+    if len(sys.argv) > 1:
+        variables = " ".join(sys.argv[1:])
+        print("Script and variables:", script_name, variables)
+
+print_args()
